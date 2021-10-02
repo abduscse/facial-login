733edfb2188141f6108f643642fa8f71e86a8443
Abdus Sattar <AG60618@ad.wellpoint.com>
2021-10-02 15:58:25 +0530

    adding joi validation

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 const ediService = require('./service');
+const joi = require('joi');
 routes = [
     {
         method: 'GET', path: '/',
@@ -6,8 +7,42 @@ routes = [
             return 'Facial login service is up and running.';
         }
     },
-    { method: 'POST', path: '/register', handler: ediService.register },
-    { method: 'POST', path: '/login', handler: ediService.login }
+    {
+        method: 'POST', path: '/register', handler: ediService.register,
+        options: {
+            validate: {
+                query: joi.object({
+                    email: joi.string().required()
+                }),
+                payload: joi.object().required()
+            },
+            response: {
+                schema: joi.object({
+                    email: joi.string().required(),
+                    message: joi.string().required()
+                }).required(),
+                failAction: 'log'
+            }
+        }
+    },
+    {
+        method: 'POST', path: '/login', handler: ediService.login,
+        options: {
+            validate: {
+                query: joi.object({
+                    email: joi.string().required()
+                }),
+                payload: joi.object().required()
+            },
+            response: {
+                schema: joi.object({
+                    email: joi.string().required(),
+                    message: joi.string().required()
+                }).required(),
+                failAction: 'log'
+            }
+        }
+    }
 ];
 
 module.exports = routes;
