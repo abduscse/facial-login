2b49508ed82e68cdb893c8770ea0565a43303ef7
Abdus Sattar <AG60618@ad.wellpoint.com>
2021-10-02 16:38:35 +0530

    schema correction

--- users.js
+++ users.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    faceId: String,
-    email: String
+    faceId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
 });
 const userModel = mongoose.model('users', userSchema);
 
