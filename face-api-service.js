f70e8e99936f401eb5acae5bb39814bcecde602e
Abdus Sattar <AG60618@ad.wellpoint.com>
2021-10-07 16:29:51 +0530

    other changes

--- face-api-service.js
+++ face-api-service.js
@@ -1,9 +1,10 @@
 const face_api = require('face-api.js');
 
 async function register(request, h) {
-    const input = request.payload.imageElement;
-    const detections = await face_api.detectAllFaces(input);
-    console.log(detections)
+    // const input = request.payload.imageElement;
+    console.log(request.payload);
+    // const detections = await face_api.detectAllFaces(input);
+    // console.log(detections)
     return 'success';
 }
 
