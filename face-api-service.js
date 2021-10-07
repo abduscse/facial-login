6a7a5e2febe93a05d07e9a91d398693a02b9034f
Abdus Sattar <AG60618@ad.wellpoint.com>
2021-10-07 15:57:34 +0530

    spell error corrected

--- face-api-service.js
+++ face-api-service.js
@@ -1,7 +1,7 @@
 const face_api = require('face-api.js');
 
 async function register(request, h) {
-    const input = request.payload.imageELement;
+    const input = request.payload.imageElement;
     const detections = await face_api.detectAllFaces(input);
     console.log(detections)
     return 'success';
